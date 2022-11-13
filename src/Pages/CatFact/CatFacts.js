d78abc05b174dbba3d3722958ddc9e3324fd72b0
gavand-sagar <gavand.sagar.s@gmail.com>
2022-11-13 11:35:13 +0530

    redux implemention

--- src/Pages/CatFact/CatFacts.js
+++ src/Pages/CatFact/CatFacts.js
@@ -2,9 +2,16 @@ import React, { useState } from 'react'
 import { Switch } from '@mui/material'
 import Header from '../../Shared/Components/Header'
 import { customGet } from '../../Utilitites/custom-fetch'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment,incrementByAmount,changeEmail } from '../../counterSlice'
+
 export default function CatFacts() {
+    const dispatch = useDispatch()
+
     const [fact, setFact] = useState('Loading...')
 
+    const count = useSelector((state) => state.counter.value)
+    const email = useSelector((state)=>state.counter.email)
 
     function loadAFact() {
 
@@ -14,16 +21,32 @@ export default function CatFacts() {
             })
     }
 
+    function changeEmailLocal(){
+        dispatch(changeEmail("John@pqr.com"))
+    }
+
+    function incrementValue(){
+        dispatch(incrementByAmount(5))
+    }
+
     return (
         <div>
             <Header />
 
-            <button onClick={loadAFact}>Get new</button>
+            email is : {email}
+            <br></br>
+
+            Count is now : <span>{count}</span>
+
+            <button onClick={incrementValue}>Increment</button>
+            <button onClick={changeEmailLocal}>Email Change</button>
+
+            {/* <button onClick={loadAFact}>Get new</button>
             <h1>Here is a fact about cats</h1>
             <p>{fact}</p>
 
             <hr></hr>
-            <Switch defaultChecked />
+            <Switch defaultChecked /> */}
         </div>
     )
 }
--- src/Pages/Questions/Questions.js
+++ src/Pages/Questions/Questions.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../../Shared/Components/Header'
 import PortalsUsername from '../../PortalsUsername'
+import { useSelector } from 'react-redux'
 export default function Questions() {
+  const count = useSelector((state) => state.counter.value)
+
   return (
     <div>
         <Header />
         <PortalsUsername>Sagar</PortalsUsername>
         Questions
+
+       the global counter is now : {count}
         </div>
   )
 }
